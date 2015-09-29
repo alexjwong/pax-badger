34b7494d239198da0d4d9f08d692a6b6d0ad4c79
Alex Wong <alexwong@bu.edu>
2015-09-28 22:35:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+pax-badger
+==========
+
+(development still in progress...!)
+
+A simple ruby script to notify you of impending availability of pax badges by monitoring the [@offical_pax](https://twitter.com/official_pax) Twitter and [paxsite.com](https://www.paxsite.com).
+
+## How to Use
+
+
+## Dependencies
++ `ruby >= 2.1.0`
++ `gem 'nogokiri'`
++ `gem 'twitter'`
+
+
+## Authors
+Alex Wong
---
+++ pax-badger.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'twitter'
+
+puts "pax-badger by alexjwong"
+puts "======================="
+puts "\n"
+
+loop do
+  # monitor..
+  puts "checking..."
+
+  # TODO: implement all of the twitter and website parsing
+  found = false
+
+  if found
+    puts "BADGES ARE COMING! Sending out notifications now!"
+  else
+    puts "No badges yet...sit tight."
+
+  end
+
+  sleep(60) # seconds
+end
