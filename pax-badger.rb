6e105f825e9c22e56309ef2173e638c5e5ef2ac9
Alex Wong <alexwong@bu.edu>
2015-09-29 22:28:27 -0400

    Add simple paxsite parser to check for the existence of class soldOut in the badges section

--- pax-badger.rb
+++ pax-badger.rb
@@ -1,16 +1,37 @@
 require 'nokogiri'
+require 'open-uri'
 require 'twitter'
+
+# TODO: Single run and background modes
+
 
 puts "pax-badger by alexjwong"
 puts "======================="
 puts "\n"
 
 loop do
+  found = false
+
   # monitor..
   puts "checking..."
 
-  # TODO: implement all of the twitter and website parsing
-  found = false
+  puts "website..."
+  paxsite = Nokogiri::HTML(open("http://east.paxsite.com"))
+  badges = paxsite.css("ul#badges")
+
+  # Local testing
+  # paxsite = Nokogiri::HTML(open("test.html"))
+  # badges = paxsite.css("ul#badges")
+  # puts badges
+  # puts badges.css("li.soldOut").empty?
+
+  if badges.css("li.soldOut").empty?
+    puts "something's different...BADGES ARE NOT SOLD OUT!"
+    found = true
+  end
+
+  # TODO: implement twitter parsing
+  # puts "...twitter"
 
   if found
     puts "BADGES ARE COMING! Sending out notifications now!"
@@ -19,5 +40,6 @@ loop do
 
   end
 
-  sleep(60) # seconds
+  break
+  # sleep(60) # seconds
 end
